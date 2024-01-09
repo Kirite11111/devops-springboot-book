d15062db80d61ed727f43857613bb8219b0a722d
Kirite11111 <eliott.garnier2810@gmail.com>
2024-01-09 18:24:08 +0100

    .sh docker run sonarqube

--- sonarqube_run.sh
+++ sonarqube_run.sh
@@ -1,3 +1,5 @@
+#docker run -d --name sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 sonarqube:latest
+
 mvn clean verify sonar:sonar \
   -Dsonar.projectKey=devops-springboot-book \
   -Dsonar.projectName='devops-springboot-book' \
